40ba0f02792626024711f97c319c002a590f529f
gregor <e@tokarev.work>
2023-04-28 15:52:55 +0300

    fix migrations

--- migrations/1668313108589-payment_options.ts
+++ migrations/1668313108589-payment_options.ts
@@ -2,12 +2,21 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class paymentOptions1668313108589 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        create table "payment_option"
+        (
+            id uuid not null primary key default uuid_generate_v4(),
+            name text not null,
+            enabled boolean default false,
+            credentials json
+        )
+    `);
     await queryRunner.query(
       `insert into payment_option(name) values ('yookassa')`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`truncate table payment_option`);
+    await queryRunner.query(`drop table payment_option`);
   }
 }
